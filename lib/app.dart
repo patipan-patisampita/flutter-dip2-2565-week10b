265b55ff4e2b203f4967d2649c3e0e0e198a58d4
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-22 16:42:33 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'contact.dart';
 import 'home.dart';
 import 'stateful.dart';
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: StateFulPage(),
+        home: ContactPage(),
     );
   }
 }
