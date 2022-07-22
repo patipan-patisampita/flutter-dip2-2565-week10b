79fc026677311e0288b5378c0cd726bc28525090
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-22 16:19:29 +0700

    update

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'stateful.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key: key);//1.constructor
+
+  @override //2.build
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: StateFulPage(),
+    );
+  }
+}
